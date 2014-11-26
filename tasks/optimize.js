9259c2a15455c5f970e3d5b6bd77f2d4d3bddd4f
tsareg <tsaregradskyi@gmail.com>
2014-11-26 13:23:19 +0200

    Added new task and modified existing

--- tasks/optimize.js
+++ tasks/optimize.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
     grunt.registerTask('optimize','Run optimization', [
         'clean:optimization',
         'merge-requirejs-configs',
-        'requirejs'
+        'requirejs',
+        'uglify'
     ]);
 };
