5d3a07d1e4b45b0b59e44ab2454e9a9d462e7c80
tsareg <tsaregradskyi@gmail.com>
2014-11-27 14:47:59 +0200

    Reverted optimize task changes

--- tasks/optimize.js
+++ tasks/optimize.js
@@ -1,8 +1,7 @@
 module.exports = function(grunt) {
     grunt.registerTask('optimize','Run optimization', [
         'clean:optimization',
-        'filterSources',
-        'generate-requirejs-optimize-options',
+        'merge-requirejs-configs',
         'requirejs'
     ]);
 };
