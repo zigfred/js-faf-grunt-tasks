411514c8af49912f5e31af39815809a945db45b9
tsareg <tsaregradskyi@gmail.com>
2014-11-27 12:04:49 +0200

    Updated tasks

--- tasks/overlay.js
+++ tasks/overlay.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
     grunt.registerTask('overlay','Prepare overlay and make new artifact from it', [
         'clean:overlay',
         'clean:maven',
+        'filterSources',
         'copy:overlay',
         'compress'
     ]);
